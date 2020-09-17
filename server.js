40300a85ba90d659b43671147025ccb6ec14a929
Kevin Miller <kmillergit@outlook.com>
2020-09-16 19:31:31 -0700

    routed html

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", { useNewUrlParser: true });
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 // Start the server
 app.listen(PORT, () => {
