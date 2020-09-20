98d60ef6c6fbc52438a53ca8a2392749d0cacc08
Kevin Miller <kmillergit@outlook.com>
2020-09-19 22:18:26 -0700

    server env variables

--- server.js
+++ server.js
@@ -22,7 +22,13 @@ require("./routes/html-routes.js")(app);
 // it will connect to the database entitled "workout" on the local host
 // Important - the workout database will not appear in Robo3T until there is data in the database!
 // This is why the seed file is useful!
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+ });
+
 
 // Start the server
 app.listen(PORT, () => {
