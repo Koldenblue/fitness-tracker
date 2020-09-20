ab97c368470bada7bb7f2fa85fa14705a4fec820
Kevin Miller <kmillergit@outlook.com>
2020-09-20 12:34:32 -0700

    cleaned code

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,18 +6,20 @@ module.exports = function(app) {
   // is last in the array
   app.get("/api/workouts", (req, res) => {
     db.Workout.find({}).sort({day:1}).then((data) => {
-      // const { exercises } = data;
-      // const totalDuration = exercises.reduce((accum, current) => (accum + current.duration), 0);
-      // const newData = Object.assign(data, totalDuration)
-      // data.setTotalDuration()
-      // res.json(newData);
       res.json(data);
     }).catch((err) => {
       res.json(err);
     })
   })
 
+  // ========== Alternative method of setting the total duration, using the reduce() method ===========
+  // const { exercises } = data;
+  // const totalDuration = exercises.reduce((accum, current) => (accum + current.duration), 0);
+  // const newData = Object.assign(data, totalDuration)
+  // res.json(newData);
 
+
+  // gets all workout documents from the workouts collection. Demonstrates alternative try / catch error handling.
   app.get("/api/workouts/range", (req, res) => {
     try {
       db.Workout.find({}, (err, data) => {
@@ -44,14 +46,12 @@ module.exports = function(app) {
 
 
   app.put("/api/workouts/:id", (req, res) => {
-    console.log(req.body)
-    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-    // finding one and updating the document object via the .save() method seems like the easiest method
+    // findOne, then updating the document object via the .save() is probably the easiest method
     // this way the docuement can be manipulated using javascript
     // rather than the $push, $set, etc. mongoDB commands
     db.Workout.findOne({_id: req.params.id}, (err, doc) => {
       if (err) throw err;
-      console.log(doc);
+
       // first we push req.body to the exercises array
       doc.exercises.push(req.body)
       // next find the total duration and add it to the document object
@@ -60,17 +60,23 @@ module.exports = function(app) {
         totalDuration += doc.exercises[i]["duration"];
       }
       doc.totalDuration = totalDuration
-      console.log("duration is " + totalDuration)
+
       // the updated document will not actually be saved without the .save() method
       doc.save()
-      console.log(doc)
       res.json(doc)
     })
+  })
+}
 
-    // ======== Alternate methods of updating documents ==================
+
+
+
+    // ======== Extra, for reference: Alternate methods of updating documents ==================
+    // As mentioned above, using the .save() method is easiest, but these methods work as well
+    //==========================================================================================
 
     // db.Workout.findOneAndUpdate({ _id: req.params.id }, { $push: {exercises: req.body} }).then((doc) => {
-    //   // with this method, the newly updated doc is not logged as updated
+    //   // with this method, the newly updated doc is not logged - console.log will still show the empty array
     //   console.log(doc);
     //   res.json(doc)
     // }).catch((err) => {
@@ -92,14 +98,9 @@ module.exports = function(app) {
       
       
       // // each req.body object added to the doc.exercise array has its own _id
-      // // _id cannot be deleted:
+      // // Demonstration: _id field cannot be deleted:
       // delete doc.exercises[doc.exercises.length - 1]['_id']
-      // console.log("======================")
+      // console.log(doc.exercises)
     // })
 
-
-
-
-  })
-}
 
